4c4649702df84e7ca3b5bc39622a8498fa89766c
Philip <powerphillg5@gmail.com>
2022-06-23 12:22:31 -0700

    will it work???

--- Move.js
+++ Move.js
@@ -11,17 +11,74 @@ class Move {
 
     getMove() {
         const head = this.#getHead();
+        const possibleMoves = this.#getPossibleMovesFromHead();
 
+        const validMoves = [];
+
+        Object.keys(possibleMoves).forEach((move) => {
+                const coordinates = possibleMoves[move];
+                if (this.#isSpaceOpen(coordinates)) {
+                    validMoves.push(move);
+                }
+            }
+        );
+
+        return validMoves[0];
+    }
+
+    #isSpaceOpen({x, y}) {
+        if (!this.#isInBounds({x, y})) {
+            return false;
+        }
+
+        if (this.#coordinateContainsSnake({x, y})) {
+            return false;
+        }
+
+        // Check for hazards
+
+
+        return true;
+    }
+
+    #coordinateContainsSnake({x, y}) {
+        const snakes = this.#snakes
+
+        for (const snake of snakes) {
+            const found = snake.body.some(snake_bod => {
+                return (snake_bod.x === x && snake_bod.y === y);
+            })
+
+            if (found) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    #isInBounds({x, y}) {
+        const {height, width} = this.#board;
+        if (y >= height || x >= width) {
+            return false;
+        }
+
+        if (y < 0 || x < 0) {
+            return false
+        }
+
+        return true;
     }
 
     #getPossibleMovesFromHead() {
-        const { x, y } = this.#snake.head;
+        const {x, y} = this.#snake.head;
 
         return {
-            up: {: y + 1,
-            down: y - 1,
-            left: x - 1,
-            right: x + 1,
+            up: {y: y + 1, x},
+            down: {y: y - 1, x},
+            left: {y, x: x - 1},
+            right: {y, x: x + 1,}
         }
     }
 
